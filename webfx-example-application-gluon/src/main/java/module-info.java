ee94041f2f962ede827ee5f8dd366d99f2150009
Bruno Salmon <dev.salmonb@gmail.com>
2022-07-12 04:28:09 +0100

    Refactored webfx-platform

--- webfx-example-application-gluon/src/main/java/module-info.java
+++ webfx-example-application-gluon/src/main/java/module-info.java
@@ -5,7 +5,7 @@ module webfx.example.application.gluon {
     // Direct dependencies modules
     requires webfx.example.application;
     requires webfx.kit.openjfx;
-    requires webfx.platform.java.boot.impl;
+    requires webfx.platform.boot.java;
     requires webfx.platform.java.scheduler.impl;
     requires webfx.platform.java.shutdown.impl;
     requires webfx.platform.shared.log.impl.simple;
